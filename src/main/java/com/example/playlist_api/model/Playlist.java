b67af3c1b357cfa6f48f873f5f030adee805bc90
janaabdelwahab <jana.abdelwahab@student.guc.edu.eg>
2026-06-23 13:59:51 +0300

    feat: Add PlaylistRepository for MongoDB operations

---
+++ src/main/java/com/example/playlist_api/model/Playlist.java
@@ -0,0 +1,4 @@
+package com.example.playlist_api.model;
+
+public class Playlist {
+}
---
+++ src/main/java/com/example/playlist_api/repository/PlaylistRepository.java
@@ -0,0 +1,12 @@
+package com.example.playlist_api.repository;
+
+import com.example.playlist_api.model.Playlist;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+
+public interface PlaylistRepository extends MongoRepository<Playlist, String> {
+
+    List<Playlist> findByUserId(String userId);
+
+}
